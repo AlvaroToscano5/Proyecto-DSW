6037f88d9de5e1b7874f6c6e0b294da4140d6be0
NahomyAnthuanet <nahomy_05_97@hotmail.com>
2017-11-30 01:58:02 -0500

    Generar Reclamo Parte 2

--- SITTPR_Web/Controllers/ReclamoController.cs
+++ SITTPR_Web/Controllers/ReclamoController.cs
@@ -9,7 +9,7 @@ using Servicios;
 namespace SITTPR_Web.Controllers {
     public class ReclamoController : Controller {
         ReclamoService reclamo = new ReclamoService();
-
+        UsuarioService usuario = new UsuarioService();
 
         public ActionResult Listar() {
             return View(reclamo.listar());
@@ -19,12 +19,28 @@ namespace SITTPR_Web.Controllers {
             ViewBag.mensaje = mensaje;
 
             ViewBag.pais = new SelectList(reclamo.listar(), "codigo", "descripcion");
+            //   ViewBag.estacion = new SelectList(estacion.listar(), "codigo", "descripcion");
 
             return View(new ReclamoEntity());
         }
 
         [HttpPost]
         public ActionResult Generar(ReclamoEntity reg) {
+
+            UsuarioEntity usu = usuario.listar().Where(u => u.dni == reg.dni).FirstOrDefault();
+
+
+            reg.usuario = usu.codigo;
+            reg.nombre = usu.nombre;
+            reg.apellido = usu.apellidos;
+            reg.tipo = usu.tipo;
+            reg.fechaReg = DateTime.Now;
+            reg.fechaAct = DateTime.Now;
+
+
+
+
+
             string msg = reclamo.generar(reg);
             return RedirectToAction("Generar", "Reclamo", new { mensaje = msg });
         }
